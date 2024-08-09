668a0d032e11ddb810fe62462ab49e88dd93d01e
Swapnil0999 <swapnil70832@gmail.com>
2024-08-09 17:47:58 +0530

    save

--- script.js
+++ script.js
@@ -169,6 +169,15 @@ function loadingAnimation() {
 
 }
 
+document.getElementById('hamburger-menu').addEventListener('click', function () {
+  var mobileNav = document.getElementById('mobile-nav');
+  if (mobileNav.style.display === 'block') {
+    mobileNav.style.display = 'none';
+  } else {
+    mobileNav.style.display = 'block';
+  }
+});
+
 locomotiveAnimation();
 navAnimation();
 page2Animation();
